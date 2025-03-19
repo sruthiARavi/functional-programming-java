efd5679a47c6744919d7ff74c92a1e13bdc92ea0
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2025-03-18 22:51:12 -0400

    Update FP03FunctionalInterfaces02.java

--- src/FP03/FP03FunctionalInterfaces02.java
+++ src/FP03/FP03FunctionalInterfaces02.java
@@ -1,10 +1,8 @@
 package FP03;
 
 import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.Random;
+import java.util.function.*;
 
 public class FP03FunctionalInterfaces02 {
     public static void main(String[] args) {
@@ -12,59 +10,91 @@ public class FP03FunctionalInterfaces02 {
 
         Predicate<Integer> isEvenPredicate = x -> x % 2 == 0;
 
-        Predicate<Integer> isEvenPredicateImpl = new Predicate<Integer>() {
-            @Override
-            public boolean test(Integer x) {
-                return x % 2 == 0;
-            }
-        };
-
         Function<Integer, Integer> squaringFunction = x -> x * x;
-
-        Function<Integer, Integer> squaringFunctionImpl = new Function<Integer, Integer>() {
-            @Override
-            public Integer apply(Integer x) {
-                return x * x;
-            }
-        };
 
         Function<Integer, String> stringOutputFunction = x -> x + " ";
 
         //Consumer<Integer> soutConsumer = x -> System.out.println(x);
         Consumer<Integer> soutConsumer = System.out::println;
 
-        Consumer<Integer> soutConsumerImpl = new Consumer<Integer>() {
-            @Override
-            public void accept(Integer x) {
-                System.out.println(x);
-            }
-        };
-
         numbers.stream()
                 .filter(isEvenPredicate)
                 .map(squaringFunction)
                 .forEach(soutConsumer);
 
-        //binary operator
-        System.out.println("sum is : " + findIntegerSum(numbers));
-
-        //Supplier
-        //Unary Operator
-    }
-
-    private static int findIntegerSum(List<Integer> numbers) {
-
+        //Sum
         BinaryOperator<Integer> integerSum = Integer::sum;
+        //BinaryOperator takes two inputs and gives an output;
+        // all the inputs and outputs are of the SAME type unlike a function
 
-        //takes two inputs and gives an output; all the inputs and outputs are of the SAME type unlike a function
-        BinaryOperator<Integer> integerSumImpl = new BinaryOperator<Integer>() {
-            @Override
-            public Integer apply(Integer x1, Integer x2) { //Function descriptor
-                return x1 + x2;
-            }
-        }; //This is an anonymous inner class
-
-        return numbers.stream()
+        int sum = numbers.stream()
                 .reduce(0, integerSum);
+
+        //binary operator
+        System.out.println("sum is : " + sum);
+
+        /*
+         ***************************
+         * OTHER FUNCTIONAL INTERFACES
+         ***************************
+         */
+
+
+        //No Input > Return something
+        //Supplier
+        //Supplier<Integer> randomIntegerSupplier = () -> 2;
+        Supplier<Integer> randomIntegerSupplier = () -> {
+            Random random = new Random();
+            return random.nextInt(1000);
+        }; //no input
+
+        System.out.println(randomIntegerSupplier.get());
+
+        //Unary Operator
+        //Binary operator operates on 2 inputs of same type and returns output of same type
+        //Unary operator takes 1 input and returns 1 output, both of same type
+        UnaryOperator<Integer> timesThreeOperator = x -> 3 * x;
+
+        System.out.println("3 times 15 is  : " +  timesThreeOperator.apply(15));
+
+        //BiPredicate
+        //2 inputs of different type of params and output type boolean
+        BiPredicate<Integer, String> biPredicate = (x,y) -> x%2 == 0 && !y.isBlank();
+        System.out.println(biPredicate.test(24, "test"));
+        System.out.println(biPredicate.test(21, ""));
+
+        //BiFunction
+        //2 different type of inputs and one different type of output allowed
+        BiFunction<Integer, String, Double> biFunction = (num, operationName) ->
+                switch (operationName.toLowerCase()) {
+                    case "square" -> num * num * 1d;
+                    case "cube" -> num * num * num * 1d;
+                    case "sqrt" -> Math.sqrt(num);
+                    default -> num * 1d;
+                };
+        System.out.println("Square of 5 : " + biFunction.apply(5, "square"));
+        System.out.println("Square root of 25 : " + biFunction.apply(25, "sqrt"));
+        System.out.println("num - " + biFunction.apply(5, "random"));
+
+        //BiConsumer
+        //takes two inputs
+        BiConsumer<Integer, String> biConsumer = (num, opName) -> {
+            System.out.println("Operation (" + opName + ") of " + num + " is : ");
+        };
+        biConsumer.accept(81, "sqrt");
+        System.out.println(biFunction.apply(81, "sqrt"));
+
+        //Built in ops using primitives - to prevent unnecessary boxing / unboxing
+        //IntBinaryOperator
+        IntBinaryOperator intBinaryOperator = Integer::sum;
+        IntBinaryOperator intBinaryOperator1 = (x, y) -> x * y;
+        //IntConsumer
+        //IntFunction
+        //IntPredicate
+        //IntSupplier
+        //IntToDoubleFunction
+        //IntToLongFunction
+        //IntUnaryOperator
     }
+
 }
